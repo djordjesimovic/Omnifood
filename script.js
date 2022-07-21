1015ba4f0237bb1dea28f74961a410230afcec24
djordjesimovic <djordjesimovic.ds@gmail.com>
2022-07-21 10:31:38 +0200

    To the testimonials

---
+++ script.js
@@ -0,0 +1,35 @@
+const icon = document.querySelector('.menu-icon');
+const responsiveMenu = document.querySelector('.responsive-menu');
+
+icon.addEventListener('click', () =>{
+    icon.classList.toggle('active');
+
+    if(icon.classList.contains('active')){
+        responsiveMenu.classList.remove('animation-remove');
+        responsiveMenu.classList.add('animation-add');
+    }
+    else{
+        responsiveMenu.classList.remove('animation-add');
+        responsiveMenu.classList.add('animation-remove');
+    }
+});
+
+
+//Sticky navbar
+
+const navbar = document.querySelector('header');
+const sticky = navbar.offsetTop;
+
+const hero = document.querySelector('.hero');
+
+window.onscroll = function(){
+
+    if(window.pageYOffset > hero.scrollHeight){
+        navbar.classList.remove('sticky-remove')
+        navbar.classList.add('sticky');
+    }
+    else{
+        navbar.classList.remove('sticky');
+        navbar.classList.add('sticky-remove')
+    }
+}
