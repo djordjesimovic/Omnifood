9f49ce3e5fd082801f02c32516f4e92a36c5c0af
djordjesimovic <djordjesimovic.ds@gmail.com>
2022-07-23 10:34:39 +0200

    Final version 1.0

--- script.js
+++ script.js
@@ -27,9 +27,11 @@ window.onscroll = function(){
     if(window.pageYOffset > hero.scrollHeight){
         navbar.classList.remove('sticky-remove')
         navbar.classList.add('sticky');
+        navbar.style.backgroundColor = '#fff'
     }
     else{
         navbar.classList.remove('sticky');
         navbar.classList.add('sticky-remove')
+        navbar.style.backgroundColor = '#fdf2e9'
     }
 }
